895df79c312da37a9f841485410e308f1d6d47de
Lala-banners <lala.l.banno@hotmail.com>
2020-10-29 17:23:44 +1100

    knights no longer collide with each other

--- Assets/Scripts/KnightMovement.cs
+++ Assets/Scripts/KnightMovement.cs
@@ -16,13 +16,8 @@ public class KnightMovement : MonoBehaviour
 
     public void EnemyMove()
     {
-        /*If distance between enemy and dragon is less than 20, soldier in shooting range
-        if (Vector2.Distance(knight.position, transform.position) < range)
-        {
-            transform.position = knight.transform.position;
-            transform.rotation = knight.transform.rotation;
-        }
-        print("Soldier is in range. SHOOT!");
-        */
+        //Move knights forward
+        
+        
     }
 }
